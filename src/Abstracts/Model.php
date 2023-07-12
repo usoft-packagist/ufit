10fac6d60b1d7bf5aafdcc2989bd3b7c6b82815d
iBekzod <erkinovbegzod.45@gmail.com>
2023-07-12 14:29:42 +0500

    query cacheable added to packet

--- src/Abstracts/Model.php
+++ src/Abstracts/Model.php
@@ -5,11 +5,11 @@ namespace Usoft\Ufit\Abstracts;
 use GeneaLabs\LaravelModelCaching\Traits\Cachable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model as Eloquent;
+use Rennokki\QueryCache\Traits\QueryCacheable;
 
 abstract class Model extends Eloquent
 {
-    use Cachable;
-    use HasFactory;
+    use HasFactory, Cachable, QueryCacheable;
     static array $scopes = [];
 
     protected static function boot()
